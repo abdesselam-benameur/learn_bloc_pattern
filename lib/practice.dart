4e70e35ac7ddd5da32d12489ff2be5d597d7ea42
Abdesselam Benameur <ia_benameur@esi.dz>
2021-01-07 07:36:48 +0100

    Add a BlocObserver

--- lib/practice.dart
+++ lib/practice.dart
@@ -14,7 +14,23 @@ class CounterCubit extends Cubit<int> {
   }
 }
 
+class SimpleBlocObserver extends BlocObserver {
+  // One added bonus of using the bloc library is that we can have access to all Changes in one place.
+  // Even though in this application we only have one Cubit, it's fairly common in larger applications
+  // to have many Cubits managing different parts of the application's state.
+
+  // Note: The internal onChange override is called first, followed by onChange in BlocObserver.
+  // Tip: In BlocObserver we have access to the Cubit instance in addition to the Change itself.
+  @override
+  void onChange(Cubit cubit, Change change) {
+    print('${cubit.runtimeType} $change');
+    super.onChange(cubit, change);
+  }
+}
+
 void main() {
+  // Note: All we need to do is extend BlocObserver and override the onChange method.
+  Bloc.observer = SimpleBlocObserver();
   CounterCubit()
     ..increment()
     ..close();
