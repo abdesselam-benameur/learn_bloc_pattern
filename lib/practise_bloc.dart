f569c59ba3aa079e2c486c880b9f26dd6c9b96d9
Abdesselam Benameur <ia_benameur@esi.dz>
2021-01-07 19:32:29 +0100

    Add mapEventToState

--- lib/practise_bloc.dart
+++ lib/practise_bloc.dart
@@ -1,12 +1,34 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-enum CounterEvent { increment }
+enum CounterEvent { increment, decrement }
 
 class CounterBloc extends Bloc<CounterEvent, int> {
   CounterBloc() : super(0);
 
   @override
-  Stream<int> mapEventToState(CounterEvent event) {
-    // TODO: implement mapEventToState
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    // Tip: async* means the function is an async generator which is capable of
+    // emitting states via the yield keyword.
+    switch (event) {
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+    }
   }
+}
+
+Future<void> main() async {
+  final bloc = CounterBloc();
+  print(bloc.state); // 0
+  bloc.add(CounterEvent.increment);
+
+  // Note: await Future.delayed(Duration.zero) is added to ensure we wait for
+  // the next event-loop iteration (allowing mapEventToState to process the increment event).
+  await Future.delayed(Duration.zero);
+
+  print(bloc.state); // 1
+  await bloc.close();
 }
